0f955e9683ea814f99195146e2008a520ec366d0
jhcreed <jordan.h.creed@moffitt.org>
2020-04-24 08:23:54 -0400

    begin saving data
    Former-commit-id: 3ba74622680c73954901183615753d25718c7747
    Former-commit-id: 181d8ab199602b102531c01fe94c8f74eb3928a0
    Former-commit-id: 1b1e83dcb4087291cae2251c1c2fdaddf3da559f
    Former-commit-id: c2a6a166810e6e21381cf1673ed4a6c7f2ac6383
    Former-commit-id: 6cf7c8df5dd3169691e1e7c50034f51df4be03e9

---
+++ R/data-GEX_Sun.R
@@ -0,0 +1,9 @@
+#' Gene expression set from Sun et al.
+#'
+#' A large expressionset containing gene expression data from Sun et al.
+#'
+#' @format A large ExpressionSet
+#' \describe{
+#'   ...
+#' }
+"GEX_Sun"
