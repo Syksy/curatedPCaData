d54330024ea27c6071be426a2ad87e12a6e6b2e7
Syksy <teemu.daniel.laajala@gmail.com>
2020-09-07 16:22:03 +0300

    Splitting Taylor into cBio and GEO subsets, adding Hieronymus
    Former-commit-id: b6ee7217b00ff673404d58cd2f48fb72450dcf81
    Former-commit-id: cfead48d6f655357bb8f2678059b5bbaabae1465
    Former-commit-id: 8ec17e58c42a36ece28744f92b183b51ee591cdd
    Former-commit-id: e8f65bee00413be3509fc745000f12ecf8d2434c
    Former-commit-id: a2d5eaac79d3b0ae1dae19fdec37417452407be3
    Former-commit-id: 564d70e955004dbe9abbc662283ac49c4769d522
    Former-commit-id: f17b68a06db4f261a1c706dfc0b5f4c18a091324
    Former-commit-id: cbf475581db47797ee083e21e8ad74a2d45016a6

--- data-raw/download-data.R
+++ data-raw/download-data.R
@@ -1,11 +1,12 @@
 # tcga data ------
+# GEX
 gex_tcga <- curatedPCaData:::generate_cbioportal(
   genes = tcga_gene_names, # All unique gene symbols
   geneticProfiles = "prad_tcga_pub_rna_seq_v2_mrna", # Omics profile
   caseList = "prad_tcga_pub_sequenced" # Case list
 )
 usethis::use_data_raw(gex_tcga, overwrite = TRUE)
-
+# CNA
 cna_tcga <- curatedPCaData:::generate_cbioportal(
   genes = tcga_gene_names,
   geneticProfiles="prad_tcga_pub_gistic",
@@ -14,20 +15,47 @@ cna_tcga <- curatedPCaData:::generate_cbioportal(
 usethis::use_data_raw(cna_tcga, overwrite = TRUE)
 
 # sun et al data -----
-gex_sun <- curatedPCaData:::generate_gex_geo()
+# GEX
+gex_sun <- curatedPCaData:::generate_gex_geo(
+  geo_code = "GSE25136"
+)
 usethis::use_data_raw(gex_sun, overwrite = TRUE)
 
 #taylor et al data -----
-gex_taylor <- curatedPCaData:::generate_cbioportal(
+# cBioPortal variant
+# GEX
+gex_cbio_taylor <- curatedPCaData:::generate_cbioportal(
   genes = tcga_gene_names,
   geneticProfiles="prad_mskcc_mrna_median_Zscores",
   caseList="prad_mskcc_sequenced"
 )
-usethis::use_data_raw(gex_taylor, overwrite = TRUE)
-
-cna_taylor <- curatedPCaData:::generate_cbioportal(
+#usethis::use_data_raw(gex_taylor, overwrite = TRUE)
+usethis::use_data_raw(gex_cbio_taylor, overwrite = TRUE)
+# CNA
+cna_cbio_taylor <- curatedPCaData:::generate_cbioportal(
   genes = tcga_gene_names,
   geneticProfiles="prad_mskcc_cna",
   caseList="prad_mskcc_sequenced"
 )
-usethis::use_data_raw(cna_taylor, overwrite = TRUE)
+#usethis::use_data_raw(cna_taylor, overwrite = TRUE)
+usethis::use_data_raw(cna_cbio_taylor, overwrite = TRUE)
+
+# GEO variant
+# GEX
+gex_geo_taylor <- curatedPCaData:::generate_gex_geo(
+  geo_code = "GSE25136"
+)
+usethis::use_data_raw(gex_geo_taylor, overwrite = TRUE)
+# CNA
+cna_geo_taylor <- curatedPCaData:::generate_cna_geo(
+  geo_code = "GSE21035"
+)
+usethis::use_data_raw(cna_geo_taylor, overwrite = TRUE)
+
+#hieronymus et al data -----
+# CNA
+cna_hieronymus <- curatedPCaData::::generate_gex_geo(
+  geo_code = "GSE54691"
+)
+usethis::use_data_raw(cna_hieronymus, overwrite = TRUE)
+
