1eed5eb6e7ece0f116871cc48cfa9af9a689ecc4
jhcreed <jordan.h.creed@moffitt.org>
2020-09-09 13:12:43 -0400

    add maes
    Former-commit-id: 106d87fcd478e09dcc5d7650f110872c49eccaf5 [formerly 30d4dad7a6d36f5f64d8329caba08cb5b8ca0306] [formerly 40f537421f7de79a36e4d716a88029ba35c30e36] [formerly 146e26b3fe0c3d3a3c1af72a08170063dc5f8da2] [formerly 16aa7b9c64db1806a6162b7428e88d00ea69a3c2] [formerly b09abd415787a4f1ba20a2b0ac298e75dcf90a53] [formerly 1d4ea58cec85bdfddcde3ba857a12a67027298ef] [formerly 0df2ea1c8d745150a9876dd9c3b479a032887f7f] [formerly 24f4c0a8f6d828cbc8ce17630ff4f4e4f284e8c4]
    Former-commit-id: 03cecb03f957c915288f93986cd76bc8cee99d96
    Former-commit-id: 68323945ca228d2283b5ec5be377999292b46e85
    Former-commit-id: 8fed733c6b7bdeda366a9dfb6f6aa4b768f14f30
    Former-commit-id: 6d00036bdb0c98be9b7612432743a2ae2ffb691d
    Former-commit-id: c51bf6f8abb5b1344c2a6e26fd85e0470316e484
    Former-commit-id: 9428e73eb1402249e50642ce69b222282b253ea7
    Former-commit-id: 4145eb68b85bed3912ad48229dbaca54c3cbf7b1
    Former-commit-id: 85bd6eaa88ad815ad378e400ad0b6ac0c1ce9a7a

--- data-raw/download-data.R
+++ data-raw/download-data.R
@@ -8,15 +8,19 @@ cna_tcga <- curatedPCaData:::generate_cbioportal(
 )
 save(cna_tcga, file="data-raw/cna_tcga.RData")
 
+mae_tcga <- create_mae()
+usethis::use_data(mae_tcga, overwrite = TRUE)
+
 # sun et al data -----
 gex_sun <- curatedPCaData:::generate_gex_geo()
 save(gex_sun, file="data-raw/gex_sun.RData")
 
+mae_sun <- create_mae(study_name = "Sun")
+usethis::use_data(mae_sun, overwrite = TRUE)
+
 #taylor et al data -----
 # insert taylor geo data code from daniel here 
 # save it to the data-raw folder using commands similar to above
-
-
 # gex_taylor <- curatedPCaData:::generate_cbioportal(
 #   genes = tcga_gene_names,
 #   geneticProfiles="prad_mskcc_mrna_median_Zscores",
@@ -28,4 +32,3 @@ save(gex_sun, file="data-raw/gex_sun.RData")
 #   geneticProfiles="prad_mskcc_cna",
 #   caseList="prad_mskcc_sequenced"
 # )
-
