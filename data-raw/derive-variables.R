a32aad741070c9b3bf345c19869a542112b9cae7
Syksy <teemu.daniel.laajala@gmail.com>
2020-11-19 21:00:27 +0200

    Downstream variables using xCell and MCP-counter and concatenate to MAE objects
    Former-commit-id: 4971109aa4f11d4fef465d95ea704411863bf3b8 [formerly ddcbe1be2ced5c48c002cc8582acd2ed57f568d8] [formerly d5898d4d85fa4746920bf9313f7d2913b5d11e6b] [formerly 3fa0f3c6310c649993cb54be4d23539d943a6048] [formerly 49b99030703aa38857123993cd5a01a7feb1325d] [formerly 74a3979eda88968983ac8015fc45192f52fe84de] [formerly a10e87d356256e2e83b6b542de25a1c645cd84d7] [formerly 6c03f99c757d0eb70558d5fdb8906f8de5228079] [formerly 0a4b74368ccb7cffdb6a8806945c4c4d03d0366b]
    Former-commit-id: 2381141ec7c6927416a9e89aa1a8988e83404468
    Former-commit-id: a365bdbb83b40fdce36218b2614aa24939288fc6
    Former-commit-id: 64ec7178e1f68a1825623c6bd3c713dce0787625
    Former-commit-id: 93c28578c4b91ac6617892863783a5eebf566314
    Former-commit-id: d4fec73f4179d3d97ea5cba92ad46910d84b3e4d
    Former-commit-id: e9320bbd6b8334df1b6b5096403de75ee525e7c8
    Former-commit-id: 1e4c2b612276c500355b8487bc4d4a055c3d5a50
    Former-commit-id: 745301e75525d288b03e151e76307133522c9c2c

---
+++ data-raw/derive-variables.R
@@ -0,0 +1,124 @@
+#####
+##
+## -- DERIVING NEW VARIABLES --
+## Set of scripts that derive new variables from the original raw data
+##
+#####
+
+#####
+##
+## xCell - immmune cell decomposition (rank-base approach; rather universal)
+##
+#####
+
+##
+# TCGA
+##
+
+# Example, xCell based on gene expression, and then concatenate this to mae_tcga
+
+# Transform from tibble to more universal formats such as a data.frame or a matrix
+tmp <- as.data.frame(immunedeconv::deconvolute(curatedPCaData::mae_tcga[["gex"]], method="xcell"))
+rownames(tmp) <- tmp$cell_type
+# Omit cell type column and store only data of cell type populations
+tmp <- as.matrix(tmp[,-1])
+# Concatenate the new results to the MAE-object
+mae_tcga <- c(curatedPCaData::mae_tcga, xcell = tmp)
+# Save the derived new 'assay' types to the mae-object
+usethis::use_data(mae_tcga, overwrite = TRUE)
+
+##
+# Taylor et al.
+##
+
+# Transform from tibble to more universal formats such as a data.frame or a matrix
+tmp <- as.data.frame(immunedeconv::deconvolute(curatedPCaData::mae_taylor[["gex"]], method="xcell"))
+rownames(tmp) <- tmp$cell_type
+# Omit cell type column and store only data of cell type populations
+tmp <- as.matrix(tmp[,-1])
+# Concatenate the new results to the MAE-object
+mae_taylor <- c(curatedPCaData::mae_taylor, xcell = tmp)
+# Save the derived new 'assay' types to the mae-object
+usethis::use_data(mae_taylor, overwrite = TRUE)
+
+##
+# Sun et al.
+##
+
+# Transform from tibble to more universal formats such as a data.frame or a matrix
+tmp <- as.data.frame(immunedeconv::deconvolute(curatedPCaData::mae_sun[["gex"]], method="xcell"))
+rownames(tmp) <- tmp$cell_type
+# Omit cell type column and store only data of cell type populations
+tmp <- as.matrix(tmp[,-1])
+# Concatenate the new results to the MAE-object
+mae_sun <- c(curatedPCaData::mae_sun, xcell = tmp)
+# Save the derived new 'assay' types to the mae-object
+usethis::use_data(mae_sun, overwrite = TRUE)
+
+
+##
+# -- Recompile and re-install package, so expanded mae-objects are up to date --
+##
+
+
+
+#####
+##
+## MCP-counter (sums of gene expression values; not sensitive to normalization method)
+##
+####
+
+##
+# TCGA
+##
+
+# Example, xCell based on gene expression, and then concatenate this to mae_tcga
+
+# Transform from tibble to more universal formats such as a data.frame or a matrix
+tmp <- as.data.frame(immunedeconv::deconvolute(curatedPCaData::mae_tcga[["gex"]], method="mcp_counter"))
+rownames(tmp) <- tmp$cell_type
+# Omit cell type column and store only data of cell type populations
+tmp <- as.matrix(tmp[,-1])
+# Concatenate the new results to the MAE-object
+mae_tcga <- c(curatedPCaData::mae_tcga, mcp_counter = tmp)
+# Save the derived new 'assay' types to the mae-object
+usethis::use_data(mae_tcga, overwrite = TRUE)
+
+##
+# Taylor et al.
+##
+
+# Transform from tibble to more universal formats such as a data.frame or a matrix
+tmp <- as.data.frame(immunedeconv::deconvolute(curatedPCaData::mae_taylor[["gex"]], method="mcp_counter"))
+rownames(tmp) <- tmp$cell_type
+# Omit cell type column and store only data of cell type populations
+tmp <- as.matrix(tmp[,-1])
+# Concatenate the new results to the MAE-object
+mae_taylor <- c(curatedPCaData::mae_taylor, mcp_counter = tmp)
+# Save the derived new 'assay' types to the mae-object
+usethis::use_data(mae_taylor, overwrite = TRUE)
+
+##
+# Sun et al.
+##
+
+# Transform from tibble to more universal formats such as a data.frame or a matrix
+tmp <- as.data.frame(immunedeconv::deconvolute(curatedPCaData::mae_sun[["gex"]], method="mcp_counter"))
+rownames(tmp) <- tmp$cell_type
+# Omit cell type column and store only data of cell type populations
+tmp <- as.matrix(tmp[,-1])
+# Concatenate the new results to the MAE-object
+mae_sun <- c(curatedPCaData::mae_sun, mcp_counter = tmp)
+# Save the derived new 'assay' types to the mae-object
+usethis::use_data(mae_sun, overwrite = TRUE)
+
+
+
+
+
+
+
+
+
+
+
