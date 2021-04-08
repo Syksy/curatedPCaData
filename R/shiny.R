318e667c5ebb62ed4a65a8c2b57acf3b5bdb39e5
Syksy <teemu.daniel.laajala@gmail.com>
2021-04-08 05:55:39 +0300

    Experimental R Shiny interface

--- R/shiny.R
+++ R/shiny.R
@@ -24,10 +24,13 @@ shiny <- function(){
 ui <- shiny::navbarPage(
 	# Title appended by current package version, sanitized from R
 	paste0("curatedPCaData v", utils::packageVersion("curatedPCaData")),
-	shiny::tabPanel("Overview",
+	# Show datasets offered in the data set
+	shiny::tabPanel("Datasets",
 		shiny::sidebarLayout(
 			# Selection menu for exported mae_objects from the package
 			shiny::sidebarPanel(
+				shiny::p("Below are the MultiAssayExperiment (MAE) objects offered in current curatedPCaData package."),
+				shiny::p("Select a dataset to further inspect elements contained in the MAE-object."),
 				shiny::uiOutput("maes")
 			),
 			# Show raw R output for an MAE object
@@ -38,11 +41,26 @@ ui <- shiny::navbarPage(
 	),
 	shiny::tabPanel("Inspect",
 		shiny::sidebarLayout(
+			# Render a menu for slots available in the currently select MAE-object
 			shiny::sidebarPanel(				
-				shiny::uiOutput("slots")
+				shiny::uiOutput("slots"),
+				#shiny::checkboxInput("tSlots", "Transpose", FALSE)
+			),
+			# Render the selected slot as a DT
+			shiny::mainPanel(
+				DT::dataTableOutput("slotDT")
+			)
+		)
+	),
+	shiny::tabPanel("Clinical",
+		shiny::sidebarLayout(
+			shiny::sidebarPanel(		
+				shiny::p("Clinical data dictionary entries")
+				#shiny::uiOutput("slots")
+				#shiny::checkboxInput("tClinical", "Transpose", FALSE)
 			),
 			shiny::mainPanel(
-				"bar"
+				DT::dataTableOutput("clinicalDT")
 			)
 		)
 	)
@@ -76,8 +94,21 @@ server <- function(input, output, session){
 	# Data slots available in currently selected data
 	output$slots <- shiny::renderUI({
 		slots <- eval(parse(text=paste0("names(MultiAssayExperiment::experiments(curatedPCaData::", input$dat,"))")))
-		shiny::selectInput("slot", "Select experiment within MAE",
+		shiny::selectInput("slot", "Select experiment/variables in MAE",
 			choices = as.list(slots)
 		)		
 	})
+	# Render a DT (mutable data table with multiple JS features)
+	output$slotDT <- DT::renderDataTable({
+		#if(input$tSlots){
+			eval(parse(text=paste0("as.data.frame(curatedPCaData::", input$dat,"[['", input$slot, "']])")))
+		#}else{
+		#	eval(parse(text=paste0("as.data.frame(t(curatedPCaData::", input$dat,"[['", input$slot, "']]))")))
+		#}
+	})
+	# Render a DT for clinical variables
+	output$clinicalDT <- DT::renderDataTable({
+		eval(parse(text=paste0("as.data.frame(as.matrix(MultiAssayExperiment::colData(curatedPCaData::", input$dat,")))")))
+	})
 }
+
