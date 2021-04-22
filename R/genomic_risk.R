20f79fb69b095196e39ca5af61c8c22ce51f897b
Syksy <teemu.daniel.laajala@gmail.com>
2021-04-22 18:35:47 +0300

    Clean up derived variables and bump up version

--- R/genomic_risk.R
+++ R/genomic_risk.R
@@ -6,7 +6,9 @@
 #'
 genomic_risk <- function(mae, 
                          object = "gex",
-                         test = c("Prolaris", "Oncotype DX", "Decipher")){
+                         test = c("Prolaris", "Oncotype DX", "Decipher"),
+                         log = TRUE # Should log-transformation be applied to the data for genomic risk score calculations; should not be utilized if data transformed already
+){
   
   prolaris_genes <- c("FOXM1", "CDC20", "CDKN3", "CDC2", "KIF11", "KIAA0101",
                       "NUSAP1", "CENPF", "ASPM", "BUB1B", "RRM2", "DLGAP5",
@@ -17,11 +19,16 @@ genomic_risk <- function(mae,
   
   oncotype_genes <- c("AZGP1", "KLK2", "SRD5A2", "FAM13C", "FLNC", "GSN", "TPM2",
                       "GSTM2", "TPX2", "BGN", "COL1A1", "SFRP4")
-  
+  # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3691249/
+  # Table 2
   decipher_genes <- c("LASP1", "IQGAP3", "NFIB", "S1PR4", "THBS2", "ANO7", 
                       "PCDH7", "MYBPC1", "EPPK1", "TSBP", "PBX1", "NUSAP1",
                       "ZWILCH", "UBE2C", "CAMK2N1", "RABGAP1", "PCAT-32", 
                       "PCAT-80", "TNFRSF19", "C6orf10")
+  # Gene name annotations / changes from original publication
+  # C6orf10 -> TSBP1
+  # PCAT-32 -> PCAT1
+  
   
   dat <- mae@ExperimentList[[object]]
   dat <- t(dat) 
@@ -44,11 +51,13 @@ genomic_risk <- function(mae,
     risk_score <- log2(risk_score)
     return(risk_score)
     
-  } else if (test == "Oncotype DX"){
+  } else if (test %in% c("Oncotype DX", "OncotypeDX")){
     
     if(length(intersect(colnames(dat), oncotype_genes)) == 12){
       
-      dat <- log2(dat)
+      if(log){
+      	dat <- log2(dat)
+      }
       
       dat$TPX2_bounded <- ifelse(dat$TPX2 < 5, 5, dat$TPX2)
       dat$SRD5A2_bounded <- ifelse(dat$SRD5A2 < 5.5, 5.5, dat$SRD5A2)
@@ -71,20 +80,21 @@ genomic_risk <- function(mae,
       
     } else {
       
-      stop("The following  required Oncotype DX genes: ", 
+      stop("The following required Oncotype DX genes: ", 
            paste(setdiff(oncotype_genes, colnames(dat)), collapse = ", "),
-           " are not in the data")
-      
+           " are not present in colnames of the data")
     }
     
-  } else {
+  } else if (test %in% "Decipher") {
     
     if(length(intersect(colnames(dat), decipher_genes)) != 19){
       
-      stop("Only ",
-           length(intersect(colnames(dat), decipher_genes)),
-           " out of 19 required genes for Decipher risk score were found")
-      
+      #stop("Only ",
+      #     length(intersect(colnames(dat), decipher_genes)),
+      #     " out of 19 required genes for Decipher risk score were found")
+      stop("The following required Decipher genes: ", 
+           paste(setdiff(decipher_genes, colnames(dat)), collapse = ", "),
+           " are not present in colnames of the data")
     }
     
     risk <- dat[,colnames(dat) %in% decipher_genes]
@@ -109,6 +119,7 @@ genomic_risk <- function(mae,
     
     return(risk_score)
     
+  } else {
+  	stop(paste("Invalid genomic risk score name:", test))
   }
-  
 }
