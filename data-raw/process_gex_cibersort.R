e119ee9dead8840d2104a1a453afca959320a783
varsha090597 <varsha97@bu.edu>
2021-07-07 18:17:53 -0700

    process gex cibersort
    Former-commit-id: 47d0cec99edadc5f5b67a82dba64d8d40179c670 [formerly f1da46e7c260689d1b88e7b62e73160190f746de [formerly 115d9d021dda199050c96413900fabe312d3acf8] [formerly 6392ff85d59841bba4ec86e9361f8de377a47208] [formerly ee837b2b47a6d9b43ae21fc8a9ae15becdab8c32] [formerly 3add6ab16ef0f7253f15501bacb31a937cf74cf2] [formerly 57480ad6505a7000097db2bbd75264d0ccef1411] [formerly 66497033b4a57b125ff3ae870d1fad4b010a9c86] [formerly 169d984c2cfaae0326ded5b90a43f85cfc3f01f1] [formerly da1b3e62b002fe0f1977c15fa3f1eca69140671f]]
    Former-commit-id: 32fee34eb5f6491ad62d18f69ad5992b686beb5e
    Former-commit-id: 237e62d4c83eec4ad21acfe776b0a9e416b42a15
    Former-commit-id: def9182d0e33d14c7dae9fa849d7f57bfa08fa94
    Former-commit-id: adad8e7429b92d73753d22221a6e44c515f6ad13
    Former-commit-id: 338b13423ffa0c52c9b3394666deb03b7b144432
    Former-commit-id: 12a7458b4b1fac1ed98c86dd329a109e993368f2
    Former-commit-id: fff2450fa5ea77113b18a32420ea5c7b68d100ab
    Former-commit-id: 1f86a3f36f881a50359551600d6779d69382a695
    Former-commit-id: 002b5d0df698b91a58e6773cd1243cb5173fbdbc
    Former-commit-id: ed4b7c99e49e24ef4b971d863282b993d7a54f1b

---
+++ data-raw/process_gex_cibersort.R
@@ -0,0 +1,93 @@
+load("data-raw/gex_sun.RData")
+gex_sun2 <- gex_sun
+gex_sun2 <- as.data.frame(gex_sun2)
+#gex_sun2 <- gex_sun2[-9561,]
+gex_sun2[,80]<-rownames(gex_sun2)
+#View(gex_sun2)
+rownames(gex_sun2) <- NULL
+gex_sun2 <- gex_sun2[,c(80,1:79)]
+gex_sun2 <- gex_sun2[-9534,]
+write.table(gex_sun2,"gex_sun.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+load("data-raw/gex_chandran.RData")
+gex_chandran2 <- gex_chandran
+gex_chandran2 <- as.data.frame(gex_chandran2)
+gex_chandran2<- gex_chandran2[complete.cases(gex_chandran2), ]
+#gex_chandran2 <- gex_chandran2[-9561,]
+gex_chandran2[,504]<-rownames(gex_chandran2)
+#View(gex_chandran2)
+rownames(gex_chandran2) <- NULL
+gex_chandran2 <- gex_chandran2[,c(504,1:503)]
+#gex_chandran2 <- gex_chandran2[-9534,]
+write.table(gex_chandran2,"gex_chandran.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+load("data-raw/gex_weiner.RData")
+gex_weiner2 <- gex_weiner
+gex_weiner2 <- as.data.frame(gex_weiner2)
+#gex_weiner2 <- gex_weiner2[-9561,]
+gex_weiner2[,839]<-rownames(gex_weiner2)
+#View(gex_weiner2)
+rownames(gex_weiner2) <- NULL
+gex_weiner2 <- gex_weiner2[,c(839,1:838)]
+#gex_weiner2 <- gex_weiner2[-9534,]
+write.table(gex_weiner2,"gex_weiner.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+load("data-raw/gex_true.RData")
+gex_true2 <- gex_true
+#gex_true2 <- as.data.frame(gex_true2)
+gex_true2<- gex_true2[complete.cases(gex_true2), ]
+#gex_true2 <- gex_true2[-9561,]
+gex_true2[,33]<-rownames(gex_true2)
+#View(gex_true2)
+rownames(gex_true2) <- NULL
+gex_true2 <- gex_true2[,c(33,1:32)]
+#gex_true2 <- gex_true2[-9534,]
+write.table(gex_true2,"gex_true.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+load("data-raw/gex_igc.RData")
+gex_igc2 <- gex_igc
+gex_igc2 <- as.data.frame(gex_igc2)
+#gex_igc2 <- gex_igc2[-9561,]
+gex_igc2[,84]<-rownames(gex_igc2)
+#View(gex_igc2)
+rownames(gex_igc2) <- NULL
+gex_igc2 <- gex_igc2[,c(84,1:83)]
+gex_igc2 <- gex_igc2[-6860,]
+write.table(gex_igc2,"gex_igc.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+load("data-raw/gex_capture_abida.RData")
+gex_capture_abida2 <- gex_capture_abida
+gex_capture_abida2 <- as.data.frame(gex_capture_abida2)
+#gex_capture_abida2 <- gex_capture_abida2[-9561,]
+gex_capture_abida2[,209]<-rownames(gex_capture_abida2)
+#View(gex_capture_abida2)
+rownames(gex_capture_abida2) <- NULL
+gex_capture_abida2 <- gex_capture_abida2[,c(209,1:208)]
+#gex_capture_abida2 <- gex_capture_abida2[-6860,]
+write.table(gex_capture_abida2,"gex_capture_abida.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+load("data-raw/gex_polyA_abida.RData")
+gex_polyA_abida2 <- gex_polyA_abida
+gex_polyA_abida2 <- as.data.frame(gex_polyA_abida2)
+#gex_polyA_abida2 <- gex_polyA_abida2[-9561,]
+gex_polyA_abida2[,267]<-rownames(gex_polyA_abida2)
+#View(gex_polyA_abida2)
+rownames(gex_polyA_abida2) <- NULL
+gex_polyA_abida2 <- gex_polyA_abida2[,c(267,1:266)]
+#gex_polyA_abida2 <- gex_polyA_abida2[-6860,]
+write.table(gex_polyA_abida2,"gex_polyA_abida.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+load("data-raw/gex_icgcca.RData")
+gex_icgcca2 <- gex_icgcca
+gex_icgcca2 <- as.data.frame(gex_icgcca2)
+#gex_icgcca2 <- gex_icgcca2[-9561,]
+gex_icgcca2[,214]<-rownames(gex_icgcca2)
+#View(gex_icgcca2)
+rownames(gex_icgcca2) <- NULL
+gex_icgcca2 <- gex_icgcca2[,c(214,1:213)]
+#gex_icgcca2 <- gex_icgcca2[-6860,]
+write.table(gex_icgcca2,"gex_icgcca.txt",sep="\t",row.names = FALSE,col.names = TRUE)
+
+
+
+
