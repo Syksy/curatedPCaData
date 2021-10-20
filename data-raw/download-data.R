2632aa6935b8251b2a505e3ef08ece5166b4000b
Syksy <teemu.daniel.laajala@gmail.com>
2021-10-21 01:03:50 +0300

    Processed Kunderfranco et al. GEX
    Former-commit-id: fe4b3ed6462934992f818d03d51c1a70a9db6f63 [formerly 9d87a086b29f397cc2bc081a6f279df845bfcb0c [formerly 80b5745cabeae8323dea924c80d85f50081d7f26] [formerly 35e8932e31946ca0e518191b5745b6ee02d937a9] [formerly 5161e15a452708ad41d9960df425eb360a8e4e2a] [formerly 1b6cfa235a3cc7ab7729e7cd767cc8c221aee791] [formerly 3c7dcca2fcceb13d3c8da916f0adec6e7fb9549c] [formerly c0b0070e5e46fb0c9cdbc8b8eab5f4847ecf8c55] [formerly a5b57cf4d1f525dbac6ac660aea8c4b8421e69da] [formerly 5b1df6c7facd1a5267a5b4c0c61f21f5b063a1c0]]
    Former-commit-id: 75ae3a02ce606338f5cafeae4fa806548f8e3edd
    Former-commit-id: cbeca4ab0e77d13d3239d123c7e912752afe9cdd
    Former-commit-id: 8aa0c1fe27195be6c4a1b1bcc5f3a59bdf4ecf0e
    Former-commit-id: e32f7fe2bf7aabdd9a8bcb820d75d26f3455e6c5
    Former-commit-id: f8cb338b5761a5d17a65a9d507c5a4baed9118ae
    Former-commit-id: a759470ec92f892b342e72d7c14360ed5a2859ad
    Former-commit-id: 14fca799fc2eadb0ffb08fbccf49a47265140939
    Former-commit-id: 2f4658bad2cdcc2c78048124628f5370653daff3
    Former-commit-id: 4114add6115ba22f2503a7f1b30eb52f8b98b3c3
    Former-commit-id: f1b0355269438447aa2e6033978ca2ec8b423c82

--- data-raw/download-data.R
+++ data-raw/download-data.R
@@ -238,12 +238,13 @@ usethis::use_data(mae_kim, overwrite = TRUE)
 
 ## - Kunderfranco et al. -
 # GEX: Agilent-012097 Human 1A Microarray (V2) G4110B (Feature Number version)
-gex_kunderfranco <- curatedPCaData:::generate_gex_geo(
+# Global LOESS normalized log ratios between the two colour arrays combined with flipped dye swap
+gex.logr_kunderfranco <- curatedPCaData:::generate_gex_geo(
 	geo_code = "GSE14206",
 	pckg = "limma",
 	cleanup = FALSE	
 )
-save(gex_kunderfranco, file = "data-raw/gex_kunderfranco.RData")
+save(gex.logr_kunderfranco, file = "data-raw/gex.logr_kunderfranco.RData")
 
 # Create MAE object
 mae_kunderfranco <- curatedPCaData:::create_mae(study_name = "kunderfranco")
