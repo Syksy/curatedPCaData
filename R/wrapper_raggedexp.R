14e9a3b2bb17f2e2750d7d510c02c2b749c78256
varsha090597 <varsha97@bu.edu>
2021-12-23 14:37:13 -0700

    wrapper function raggedexp
    Former-commit-id: 35f52294d5caca0817e84103e9677a7f9b833abc [formerly 188b722554bce456581cfc2e54747e6294e54e13 [formerly 0a087819052a2cb76a6d63d0e6eff83f2e30b4a4] [formerly 6311421f61107674a97115d27da822794aba1cab] [formerly 422dd49d7d56e66430aa22cc56f28018e9605cb9] [formerly 1e571712b73697cc1e77ee0e6e127e60fea9cab0] [formerly fb2f80869a953ac176d67b48efaff8ad9cdb4779] [formerly 22a4432d732026c8c6afc41ea26cef8a77a23625] [formerly 4fd4859ba740a1fb381a5da8d97ef025f72b0fb9] [formerly 43d5c8e3a1799c63999b3add37262d9b2c648d74]]
    Former-commit-id: 40b21f7b3223f50e1fcc70ce24e752c5d70f511f
    Former-commit-id: 725ed7cb7d744cbd69606baab053063ee295901c
    Former-commit-id: 00fe5c786bb91db8bc72bc718089a2d2b7469a8f
    Former-commit-id: 2c0b6f191d85f3e5841ff16737d424d3641215a7
    Former-commit-id: 7dee172053af5824faf6dee0ecd0126fd06451a8
    Former-commit-id: d61af240490a8e9809db32a25685a6c9a8f12e96
    Former-commit-id: e88dd1e393c515cbf877fddc045fd89d3fec2f7d
    Former-commit-id: ff6fd8c697f7d0577c77ac3e521304a9c76785e6
    Former-commit-id: 5138592c615028d18b588e4a5a0083e19e8cc11f
    Former-commit-id: dd75296c89fd1066478f967a42392bc70a22a037

---
+++ R/wrapper_raggedexp.R
@@ -0,0 +1,32 @@
+wrapper_raggedexp<-function(ragexp){
+I=sparseAssay(ragexp,"Variant_Classification")
+I=as.data.frame(I)
+I$gene=rownames(I)
+#I=I[,c(44,1:43)]
+I$gene=gsub("\\..*","",I$gene)
+
+
+v=I %>% 
+  group_by(gene) %>% 
+  summarise_all(toString)
+
+v=as.data.frame(v)
+
+b <- data.frame(lapply(v, function(x) {
+  gsub("NA, NA, NA", "NA", x)
+}))
+D<-data.frame(lapply(b, function(x) {
+  gsub("NA, NA", "NA", x)
+}))
+E<-data.frame(lapply(D, function(x) {
+  gsub("NA,", "", x)
+}))
+G<-data.frame(lapply(E, function(x) {
+  gsub(", NA", "", x)
+}))
+
+rownames(G)=G$gene
+G <- G[ , ! names(G) %in% "gene"]
+
+return(G)
+}
