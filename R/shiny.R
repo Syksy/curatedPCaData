50ad107564cfc119c49fc8ef9aebd1053ceafb66
Syksy <teemu.daniel.laajala@gmail.com>
2022-03-24 19:55:10 +0200

    v0.8 with DeMixT
    Former-commit-id: 28014f53ec45c892cfab738bd25250be255a9556 [formerly c4cf63a9d95ba457eb56a957410cf7da2e571c8f [formerly 7f7e98a0b6d8fcd2f0c7b89ad3c0bc7c60e849b4] [formerly a1e6e7902a5a6b369ebe9fea1f15831d0028fc58] [formerly f023035501b3077c586617d9b9ff73edba08097d] [formerly ca844ed5467dba34404e02ba6be2055d8b8f4646] [formerly ab5e428d3d64793103e7f611c4f3556ce013ced5] [formerly d73358d504cb9c67a95b8eff66f18a4c825bef54] [formerly 48f5d30784175341bf78814bae660f10fcb7ae4d] [formerly c704f1714109d7a8442936c0f80b6342132f5d56]]
    Former-commit-id: d0d2dea217fefcfd72064e1054f37b66530362d8
    Former-commit-id: b7688ff0252d1c8acf66243ba8a765e6f2cd0074
    Former-commit-id: d352ee1dd5ac012226e8b44c19bd55ffc25ba66c
    Former-commit-id: 4a88e6b75b64e69b78cd73aa5a6d2630c073a94d
    Former-commit-id: 42cbe14661607eac720773b949a3bb6209f674a5
    Former-commit-id: a92d4da590ad7a31181dbd9d3b4a84367b3b1aa0
    Former-commit-id: 22ffb25149f4b579ccd5baf3877f02bae51cec73
    Former-commit-id: f71e32fb9de747c196689367c0eb45bbf952d850
    Former-commit-id: 229a21a73451c3428a55e50866425f910b448b77
    Former-commit-id: d508863b20bfac5ac5ca74013b3d7cf4576b1958

--- R/shiny.R
+++ R/shiny.R
@@ -8,109 +8,95 @@
 #'
 #' Description
 #'
-#' @rdname shiny
+#' @rdname shinyPCa
 #'
 #' @export
-shiny <- function(){
-	app <- shiny::shinyApp(ui = curatedPCaData:::ui, server = curatedPCaData:::server)
+shinyPCa <- function(){
+	app <- shiny::shinyApp(
+		ui = shiny::navbarPage(
+			# Title appended by current package version, sanitized from R
+			paste0("curatedPCaData v", utils::packageVersion("curatedPCaData")),
+			# Show datasets offered in the data set
+			shiny::tabPanel("Datasets",
+				shiny::sidebarLayout(
+					# Selection menu for exported mae_objects from the package
+					shiny::sidebarPanel(
+						shiny::p("Below are the MultiAssayExperiment (MAE) objects offered in current curatedPCaData package."),
+						shiny::p("Select a dataset to further inspect elements contained in the MAE-object."),
+						shiny::uiOutput("maes")
+					),
+					# Show raw R output for an MAE object
+					shiny::mainPanel(
+						shiny::verbatimTextOutput("dat_verb")
+					)
+				)
+			),
+			shiny::tabPanel("Inspect",
+				shiny::sidebarLayout(
+					# Render a menu for slots available in the currently select MAE-object
+					shiny::sidebarPanel(				
+						shiny::uiOutput("slots"),
+						#shiny::checkboxInput("tSlots", "Transpose", FALSE)
+					),
+					# Render the selected slot as a DT
+					shiny::mainPanel(
+						DT::dataTableOutput("slotDT")
+					)
+				)
+			),
+			shiny::tabPanel("Clinical",
+				shiny::sidebarLayout(
+					shiny::sidebarPanel(		
+						shiny::p("Clinical data dictionary entries")
+						#shiny::uiOutput("slots")
+						#shiny::checkboxInput("tClinical", "Transpose", FALSE)
+					),
+					shiny::mainPanel(
+						DT::dataTableOutput("clinicalDT")
+					)
+				)
+			)
+		), 
+		server = function(input, output, session){
+			# Need to load select options on the run
+			updateSelectInput(session, "dat",
+				choices = as.list(utils::data(package="curatedPCaData")$results[,"Item"])
+			)
+			# outputs
+			output$dat_verb <- shiny::renderPrint({
+				if(is.null(input$dat) | input$dat == ""){
+					#"Please select a MAE-object exported from the curatedPCaData-package on the left side."
+					print(curatedPCaData:::as.named.list(utils::data(package="curatedPCaData")$results[,"Item"]))
+				}else{
+					eval(parse(text=paste0("curatedPCaData::", input$dat)))
+				}
+			})
+			# MAE set extracted from latest package data listing
+			output$maes <- shiny::renderUI({
+				shiny::selectInput("dat", "Select MAE-object",
+					choices = as.list(utils::data(package="curatedPCaData")$results[,"Item"])
+				)
+			})
+			# Data slots available in currently selected data
+			output$slots <- shiny::renderUI({
+				slots <- eval(parse(text=paste0("names(MultiAssayExperiment::experiments(curatedPCaData::", input$dat,"))")))
+				shiny::selectInput("slot", "Select experiment/variables in MAE",
+					choices = as.list(slots)
+				)		
+			})
+			# Render a DT (mutable data table with multiple JS features)
+			output$slotDT <- DT::renderDataTable({
+				#if(input$tSlots){
+					eval(parse(text=paste0("as.data.frame(curatedPCaData::", input$dat,"[['", input$slot, "']])")))
+				#}else{
+				#	eval(parse(text=paste0("as.data.frame(t(curatedPCaData::", input$dat,"[['", input$slot, "']]))")))
+				#}
+			})
+			# Render a DT for clinical variables
+			output$clinicalDT <- DT::renderDataTable({
+				eval(parse(text=paste0("as.data.frame(as.matrix(MultiAssayExperiment::colData(curatedPCaData::", input$dat,")))")))
+			})
+		}
+	)
 	shiny::runApp(app)
 }
-
-#' Shiny UI side for browser
-#'
-#' Description
-#'
-#' @noRd
-#' @keywords internal
-ui <- shiny::navbarPage(
-	# Title appended by current package version, sanitized from R
-	paste0("curatedPCaData v", utils::packageVersion("curatedPCaData")),
-	# Show datasets offered in the data set
-	shiny::tabPanel("Datasets",
-		shiny::sidebarLayout(
-			# Selection menu for exported mae_objects from the package
-			shiny::sidebarPanel(
-				shiny::p("Below are the MultiAssayExperiment (MAE) objects offered in current curatedPCaData package."),
-				shiny::p("Select a dataset to further inspect elements contained in the MAE-object."),
-				shiny::uiOutput("maes")
-			),
-			# Show raw R output for an MAE object
-			shiny::mainPanel(
-				shiny::verbatimTextOutput("dat_verb")
-			)
-		)
-	),
-	shiny::tabPanel("Inspect",
-		shiny::sidebarLayout(
-			# Render a menu for slots available in the currently select MAE-object
-			shiny::sidebarPanel(				
-				shiny::uiOutput("slots"),
-				#shiny::checkboxInput("tSlots", "Transpose", FALSE)
-			),
-			# Render the selected slot as a DT
-			shiny::mainPanel(
-				DT::dataTableOutput("slotDT")
-			)
-		)
-	),
-	shiny::tabPanel("Clinical",
-		shiny::sidebarLayout(
-			shiny::sidebarPanel(		
-				shiny::p("Clinical data dictionary entries")
-				#shiny::uiOutput("slots")
-				#shiny::checkboxInput("tClinical", "Transpose", FALSE)
-			),
-			shiny::mainPanel(
-				DT::dataTableOutput("clinicalDT")
-			)
-		)
-	)
-)
-
-#' Shiny server side
-#'
-#' Description
-#'
-#' @noRd
-#' @keywords internal
-server <- function(input, output, session){
-	# Need to load select options on the run
-	updateSelectInput(session, "dat",
-		choices = as.list(utils::data(package="curatedPCaData")$results[,"Item"])
-	)
-	# outputs
-	output$dat_verb <- shiny::renderPrint({
-		if(is.null(input$dat) | input$dat == ""){
-			#"Please select a MAE-object exported from the curatedPCaData-package on the left side."
-			print(curatedPCaData:::as.named.list(utils::data(package="curatedPCaData")$results[,"Item"]))
-		}else{
-			eval(parse(text=paste0("curatedPCaData::", input$dat)))
-		}
-	})
-	# MAE set extracted from latest package data listing
-	output$maes <- shiny::renderUI({
-		shiny::selectInput("dat", "Select MAE-object",
-			choices = as.list(utils::data(package="curatedPCaData")$results[,"Item"])
-		)
-	})
-	# Data slots available in currently selected data
-	output$slots <- shiny::renderUI({
-		slots <- eval(parse(text=paste0("names(MultiAssayExperiment::experiments(curatedPCaData::", input$dat,"))")))
-		shiny::selectInput("slot", "Select experiment/variables in MAE",
-			choices = as.list(slots)
-		)		
-	})
-	# Render a DT (mutable data table with multiple JS features)
-	output$slotDT <- DT::renderDataTable({
-		#if(input$tSlots){
-			eval(parse(text=paste0("as.data.frame(curatedPCaData::", input$dat,"[['", input$slot, "']])")))
-		#}else{
-		#	eval(parse(text=paste0("as.data.frame(t(curatedPCaData::", input$dat,"[['", input$slot, "']]))")))
-		#}
-	})
-	# Render a DT for clinical variables
-	output$clinicalDT <- DT::renderDataTable({
-		eval(parse(text=paste0("as.data.frame(as.matrix(MultiAssayExperiment::colData(curatedPCaData::", input$dat,")))")))
-	})
-}
-
