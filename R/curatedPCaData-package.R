a06457cf429411090eb2e612bbc2e008f9ce67b4
Syksy <teemu.daniel.laajala@gmail.com>
2022-07-05 23:03:04 +0300

    R CHECK debugging and wrapper docs

--- R/curatedPCaData-package.R
+++ R/curatedPCaData-package.R
@@ -1,9 +1,11 @@
 #' @keywords internal
 #' @import S4Vectors
 #' @import MultiAssayExperiment
+#' @import RaggedExperiment
 #' @importFrom methods slot
 #' @importFrom stats median
 #' @importFrom utils read.table
+#' @importFrom magrittr %>%
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
