ae120a0d64005b514f50dd9414425cc77191f8d7
Syksy <teemu.daniel.laajala@gmail.com>
2023-04-27 00:46:38 +0300

    Main ExperimentHub data fetch function
    Former-commit-id: c5d8683b24a38264037baa20ad4a3f9581e5d185
    Former-commit-id: 877e5c211176942d388c0a6af3e719172f24d6fb
    Former-commit-id: f362909fdd868279fb50fb81627b9631babd67d8
    Former-commit-id: 21e45a00b3f56c425a7c32ee1a1e759d3b951d55
    Former-commit-id: dfac8057d47c199e62edd8b8ab5e4f85181c75d0
    Former-commit-id: 9766a809a4834e790541e8b804875e17c7ea7483
    Former-commit-id: eb3b56ce669ab0e3d2917c35ad245a5089a55e73
    Former-commit-id: 720218f803edff76912208c1f4edbf3c84ee94b6
    Former-commit-id: 59eb1c8edc3331f9bcea07743ab5e14d17e881b5
    Former-commit-id: 7a2214939d59544ce333be01309375fa9c3c517e
    Former-commit-id: c38c24aabb0d2c09fb80ecf5c692d645fa7a5238

---
+++ R/getpca.R
@@ -0,0 +1,247 @@
+###
+#
+# getPCa:
+# Main function and additional support functions for fetching Prostate Cancer (PCa) 
+# datasets from ExperimentHub curated via curatedPCaData
+#
+###
+
+.conditionToIndex <- function(startVec, testVec, FUN) {
+  logmat <- vapply(startVec, FUN, logical(length(testVec)))
+  apply(logmat, 1L, any)
+}
+
+.getResources <- function(ExperimentHub, resTable, verbose) {
+  fileNames <- stats::setNames(resTable[["RDataPath"]], resTable[["Title"]])
+  resources <- lapply(resTable[["Title"]], function(res) {
+    if (verbose)
+      message("Working on: ", gsub("\\.rds", "", basename(res)))
+    query(ExperimentHub, res)[[1L]]
+ 
+  })
+  
+  names(resources) <- resTable[["Title"]]
+  resources
+}
+
+.test_eh <- function(...) {
+  tryCatch({
+    ExperimentHub(...)
+  }, error = function(e) {
+    emsg <- conditionMessage(e)
+    if (grepl("Timeout", emsg))
+      warning("[experimenthub.bioconductor.org] timeout, localHub=TRUE",
+              call.=FALSE)
+    ExperimentHub(..., localHub = TRUE)
+  })
+}
+
+#' Create a MultiAssayExperiment from specific assays and cohorts
+#'
+#' @description curatedPCaData probides \linkS4class{MultiAssayExperiment} 
+#' container objects that are constructed from ExperimentHub.
+#' User provides PCa data set name (see list XXXX) and YYYY.
+#'
+#' @details This function will check against available resources in
+#' ExperimentHub. 
+#' Use the \code{dry.run = FALSE} to download remote datasets and
+#' build an integrative \linkS4class{MultiAssayExperiment} object.
+#' For a list of datasets, see the XXXX
+#'
+#' @param dataset character() of PCa cancer cohort names
+#'     (e.g., `abida`)
+#'
+#' @param slots character() A vector of PCa assays. If not included, returns all
+#'     available for the selected dataset;
+#'     see below for more details
+#'
+#' @param timestamp character(1) "20230215" indicating the data version to obtain 
+#'     from `ExperimentHub`. See `version` section details. XXXXXXXXXXXXXXX
+#'
+#' @param ... Additional arguments passed on to the
+#'     \code{\link[ExperimentHub:ExperimentHub-class]{ExperimentHub}}
+#'     constructor
+#'
+#' @param verbose logical(1) Whether to show the dataset currenlty being
+#'     (down)loaded (default TRUE)
+#'
+#' @section Available Assays:
+#'
+#' Thelist of ExperimentList assay names and their descriptions.
+#' These assays can be entered as part of the \code{slots} argument in the 
+#' main function. 
+#' \preformatted{
+#'
+#' ExperimentList data types   Description
+#' ----------------------------------------------------------------------------
+#' 
+#'   gex.rma	               Gene expression values
+#'   gex.logq              	   
+#'   gex.relz			
+#'	 gex.logr
+#'   gex.rsem.log
+#'	 cna.gistic				   Copy number alteration
+#'   cna.logr
+#'   mut					   Mutation
+#'   cibersort				
+#'   xcell
+#'   epic
+#'   quantiseq
+#'   mcp
+#'   estimate
+#'   scores      
+#'
+#' @section timestamp: XXXXXX
+#' The timestamp is updated in case the data is updated. In this case, this 
+#' section describes the changes made for new timestamps.
+#'
+#' @seealso curatedPCaData-package
+#'
+#' @return a \linkS4class{MultiAssayExperiment} of the specified assays and
+#' cancer code
+#'
+#' @examples
+#'
+#' curatedPCaMAE(
+#'     dataset = "abida", timestamp = "20230215"
+#' )
+#'
+#'
+#' @md
+#'
+#' @export getPCa
+getPCa <-  function(
+	# Dataset name
+	dataset, 
+	# Data slots to retrieve (i.e. user can subset to just desired data)
+	slots, 
+	# Timestamps of data from ExperimentHub; allowed values: '20230215'
+	timestamp,
+	# Verbosity
+	verbose = TRUE, 
+	# Additional parameters
+	...
+)
+{
+	if(missing(dataset)){
+		stop("Select dataset; see ?curatedPCaData") # XXXX Where is the list?
+	}
+
+	if(length(dataset)>1){
+		stop("Select only one dataset at a time.")
+	}
+
+	dataset <- tolower(dataset)  # datasets are saved as lower case
+
+	## Change this if new update (with new timestamp) is added
+	if(missing(timestamp)){
+		timestamp <- "20230215"
+	}
+
+	# Update this if more timestamps becomes available
+	if (any(!timestamp %in% c("20230215"))){
+	  stop("'timestamp' contains a timestamp that is not '20230215'; see '?curatedPCaData'")}
+
+	assays_file <- system.file("extdata", "metadata.csv", package = "curatedPCaData", mustWork = TRUE)
+	assay_metadat <- read.csv(assays_file, stringsAsFactors = FALSE)
+
+	# Separate names Title in assay_metadat to get dataset, assay and timestamp separately
+	eh_assays <- assay_metadat[["Title"]]
+	eh_assays_sep <- t(as.data.frame(strsplit(eh_assays,"_")))
+
+	# Get only requested dataset
+	dataId <- which(eh_assays_sep[,1]==dataset)
+
+	if(length(dataId)==0){
+	  stop("Dataset name is incorrect or not available.")
+	}
+
+	eh_assays_sep <- eh_assays_sep[dataId,]
+
+	assaysAvail <- unique(eh_assays_sep[,2])  # Get available assays for selected dataset
+
+	# Select user specified assays (or all if nothing specified)
+	if(missing(slots)){ # If nothing specific requested, return all
+
+	}else{
+	   if(any(!slots %in% assaysAvail)){ # If user asks for something that is not available, 
+		  stop(paste0(c("At least one of asked slots is not available. The available slots for this dataset are:",assaysAvail),collapse="  "))
+	   }else{ # Select only requested assays
+		  assaysAvail <- unique(c(slots,"colData","sampleMap"))
+	   }
+	}
+
+	# Select assays by timestamp request. If more versions are added this has to be updated.
+	# sampleMap is always selected by the latest timestamp given. If not available, latest is used.
+	codeAssay <- c()
+	latest <- as.character(max(as.numeric(timestamp)))
+	
+	for(i in assaysAvail){
+		assayId <- which(eh_assays_sep[,2]==i)
+		availableTimestamp <- eh_assays_sep[assayId,3]
+
+		if(i == "sampleMap"){  # Use latest given timestamp for sampleMap, if not available, use latest in total
+		  if(latest %in% availableTimestamp){
+			selectedTimestamp <- latest
+		  }else{
+			selectedTimestamp <- as.character(max(as.numeric(availableTimestamp)))
+		  }
+		}else{
+		  # Go through the user given timestamp request and get the first match, if no match, give latest
+		  nomatch <- FALSE
+		  n <- 1
+		  while(!nomatch){
+			# Select the first match, if no match, select latest
+			if(timestamp[n] %in% availableTimestamp){
+			  nomatch <- TRUE
+			  selectedTimestamp <- timestamp[n]
+			}else{
+			  n<-n+1
+			  if(n>length(timestamp)){
+				nomatch <- TRUE
+				selectedTimestamp <- as.character(max(as.numeric(availableTimestamp)))
+			  }
+			}
+		  }
+		}
+
+		codeAssay <- c(codeAssay,paste0(dataset,"_",i,"_",selectedTimestamp))
+	}
+	
+	# Get indices of selected dataset_assay_timestamp combos
+	fileIdx <- .conditionToIndex(codeAssay, eh_assays, function(x) startsWith(eh_assays, x))
+	fileMatches <- assay_metadat[fileIdx, c("Title", "DispatchClass")]
+
+	# Sanitycheck
+	if (!length(nrow(fileMatches))){
+		stop("Cancer and data type combination(s) not available")
+	}
+
+	eh <- .test_eh()
+
+	# Get the data
+	assay_list <- .getResources(
+		eh, assay_metadat[fileIdx, c("Title", "RDataPath")], verbose
+	)
+
+	# Get experiments and name them only by the assay/experiment/etc name
+	# Omit colData and sampleMap since they are added separately
+	cD_idx <- which(grepl("colData", names(assay_list), fixed = TRUE))
+	sM_idx <- which(grepl("sampleMap", names(assay_list), fixed = TRUE))
+
+	# Final sanity check
+	if(length(cD_idx)!=1 | length(sM_idx)!=1){
+		stop("Either colData or sampleMap is missing for this dataset and the MAE cannot be constructed, or there are multiple with same timestamp.")
+	}
+
+	eh_experiments <- ExperimentList(assay_list[-c(cD_idx,sM_idx)])
+	names(eh_experiments) <- gsub("(^[a-z]*)_(.*)_(.*)", "\\2", names(eh_experiments))
+
+	# Return MAE
+	MultiAssayExperiment::MultiAssayExperiment(
+		experiments = eh_experiments,
+		colData = assay_list[cD_idx][[1]],
+		sampleMap = assay_list[sM_idx][[1]]
+	)
+}
+
