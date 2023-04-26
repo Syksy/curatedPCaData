9c65e1dce0617bbc070bf012c95185f343efd67c
Syksy <teemu.daniel.laajala@gmail.com>
2023-04-26 23:47:06 +0300

    Description of original data generation with curatedPCaData
    Former-commit-id: b64e0bfd3352cac8f1f638d7be65f8d1c6878a4d [formerly b25390eefccaeb1d6f5186eb7e95e0f6ce7d043a] [formerly e0eb165cdeeb63a757502363f74dfbf8f35a983b] [formerly 7ac9765127084c9f1756d92b3e48df6993a3b347] [formerly 5ad97f7a74e3e5f0da35443daf0356c76da247a9] [formerly 31fb5cb8ee9c09793784e407e3097c7948d5aa91] [formerly fd8ddb5e9357e02510e30d1d034a825a7b8f33bc] [formerly e8c526e237839a38fcc2550b981a2e0a5f74b477] [formerly 54c7a86d3520dd6c7ad6979d35fd4aa4253dee70] [formerly 3c8c3309623ac9adfea33676a9655f308cc811fb] [formerly 6162d3c6921e969d0b9c7ec80ac14ac7103a7227] [formerly 20b08d291330b441caacbbba4421f98f4fb77f82]
    Former-commit-id: ef52a45fd120c2ad1a4880f0ecc19f4ff298fbac
    Former-commit-id: d011ff2b364e8509c4550c10a753a64a2f86f757
    Former-commit-id: 5cedcb448ee52f1707273bc59ce65d3f4550f6a5
    Former-commit-id: 778dd6d5f7726272308da5c9ea653e7b391089b1
    Former-commit-id: 8a9ed8314fa2a6d48aff88643fb2ed340d73095b
    Former-commit-id: ffd7125306b035fc5a6b5dd754f14a5866a8f2a8
    Former-commit-id: e3fe386a9db8ac5cc12dde24ee7715b015c79e92
    Former-commit-id: a13d45ac523c1dacf0deb7e4e3d0c6884268a7fc
    Former-commit-id: 3be3cc501f0e5b5ceea39ed0e663153f24fadc75
    Former-commit-id: c4af3c27acebc77a60847b299f2c535d98ef0d30
    Former-commit-id: f7bf92636190f689eb7675b96904b3ca151cc5e0

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,2 +1,33 @@
-# Steps involved in making the curatedPCaData ExperimentHub-objects
+## Steps involved in making the curatedPCaData ExperimentHub-objects
 
+## From the package root:
+
+# ../data-raw/download-clinical.R
+## -> Downloads the clinical data matrices from multiple sources (GEO, cBioPortal, cgdsr, ...)
+##   and combines them into colData-suitable data.frames for MultiAssayExperiments.
+
+# ../data-raw/download-data.R
+## -> Download and harmonize raw data; for example, for gene expression CEL data this will call the 
+##    correct functions for using GEOquery, and subsequent data processing such as RMA-normalization.
+
+# ../data-raw/data-curatedPCaData_genes.R
+## -> Extract hugo symbols from biomaRt, as well as specific array annotations if available.
+##    These are used when constructing e.g. gene-level expression matrices.
+
+# At this stage, the version 1 of curatedPCaData should be build using R CMD build.
+# The correct MAE-objects have been stored and in prior versions were offered as LazyLoad'ed MAE-objects.
+# After this, derived variables are extracted
+
+# ../data-raw/derive-variables.R
+## -> This script assumes the correct MAE-objects are stored inside ../data/-folder of the package.
+##    The MAE-objects are loaded from there, and then the derived variables (such as immune deconvolution
+##    results and risk scores) are calculated and appended as slots/assays into the existing MAE-objects.
+
+# After above steps, original MultiAssayExperiments were stored in ../data/ of the package.
+# In order to make the package compatible with ExperimentHub, these MAE-objects were dissected and an
+# additional metadata sheet is automatically generated:
+
+# write.csv(curatedPCaData:::export_metadata(timestamp = "20230215"), file="metadata.csv", quote=TRUE, row.names=FALSE)
+## -> Creates the ExperimentHub-friendly metadata.csv
+
+# 
