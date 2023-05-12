a286549d21d770fd3d9a8cc4337818df62ac050b
Syksy <teemu.daniel.laajala@gmail.com>
2023-05-12 16:02:26 +0300

    Move old raw roxygen2 for LazyLoaded data
    Former-commit-id: 8298819043f3807b2cbb3bc8e9d5d35007dfe2df
    Former-commit-id: 83d782b3c2f5bcd63af9b8728a3ac9cb9d0798a0
    Former-commit-id: 6f60539f6c90d5c9f5b9cf5525fbc19048509563
    Former-commit-id: 30aeb386359ac8f6a5c09c865cb46b36b11ba8fd
    Former-commit-id: ecb72a59bbee1638d09168c209b5e0af99728f0e
    Former-commit-id: 3f1582fdc4de26f7df91349274d192ae1325eaad
    Former-commit-id: 27f83b0e85306bdefee8375ad18c877b753298fa
    Former-commit-id: c2b223a25b3154ec9c5cfa102d83f6a8a9708c1b
    Former-commit-id: bd010bb414b6b277d87fd015562eaeb7b99cfb55
    Former-commit-id: cdd62edf13aed328df4b00242e2f57478f649325
    Former-commit-id: 59e5ad2bed879c82f025b3d1646cb41713631d52

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Template for curatedPCaData clinical fields
+#'
+#' This data frame contains the clinical fields which were the aim for extraction when gathering metadata for each dataset. 
+#' Variable types, ranges, and description is reported.
+#'
+#' @examples
+#' head(template_prad)
+"template_prad"
